eefe2edcb92c24ff72c415094deb916e36d568b9
Karen Rustad <karen.rustad@gmail.com>
2013-10-20 00:13:12 -0700

    pep8 and style changes, added requests

--- cached.py
+++ cached.py
@@ -1,31 +1,37 @@
 #!/usr/bin/env python
-
 import os
-import subprocess
+# import subprocess
+import requests
 from lxml import etree
 
-DMV_OFFICES_URL = ""
 
-def download(refresh = False):
+DMV_OFFICES_URL = "http://apps.dmv.ca.gov/fo/offices/appl/fo_data/foims_XmlFOdata.xml"
+DMV_DATA_URL = "http://apps.dmv.ca.gov/fo/offices/apps/foGeoXml_v2.jsp?foNumb="
+
+def download_offices(refresh=False):
+  '''Download office ID data'''
   outfile = "offices.xml"
   try:
     os.stat(outfile)
     print 'Using cached copy of', outfile
-  except Exception, e:
+  except OSError:
     refresh = True
   if refresh:
-    subprocess.check_call(['curl','-o',outfile,'http://apps.dmv.ca.gov/fo/offices/appl/fo_data/foims_XmlFOdata.xml'])
-def downloadWait(index,refresh = False):
-  outfile = "xmlhttprequest_%04d.xml" % index
-  try:
-    os.stat(outfile)
-    print 'Using cached copy of', outfile
-  except Exception, e:
-    refresh = True
-  if refresh:
-    subprocess.check_call(['curl','-o',outfile,'http://apps.dmv.ca.gov/fo/offices/apps/foGeoXml_v2.jsp?foNumb='+str(index)])
+    # subprocess.check_call(['curl', '-o', outfile, 'http://apps.dmv.ca.gov/fo/offices/appl/fo_data/foims_XmlFOdata.xml'])
+    r = requests.get(DMV_OFFICES_URL)
+    assert r.status_code == 200, "HTTP error %s" % (r.status_code,)
+    fout = open(outfile, 'w')
+    for line in r.text:
+      fout.write(line)
 
-def getNums():
+def get_wait_time(office_id, refresh=True):
+  '''Downloads data for a given office ID number and returns the wait time'''
+  r = requests.get(DMV_OFFICES_URL + str(office_id))
+  assert r.status_code == 200, "HTTP error %s" % (r.status_code,)
+  return parse_wait(r.text)
+
+def parse_office_nums():
+  '''Returns a list of the valid ID numbers for DMV offices'''
   foims = open('offices.xml','rb')
   tree = etree.parse(foims)
   foims.close()
@@ -42,21 +48,18 @@ def getNums():
       continue
     results.append(int(child.attrib['num'],10))
   return results
-def getWait(index):
-  infile = "xmlhttprequest_%04d.xml" % index
-  xmlhr = open(infile,'rb')
-  tree = etree.parse(xmlhr)
-  xmlhr.close()
+
+def parse_wait(data):
+  '''Returns the non-appointment wait time in minutes'''
+  tree = etree.parse(data)
   root = tree.getroot()
   hour, mins = root.find('./fo_office/nonAppt').text.split(':')
   return int(hour,10) * 60 + int(mins,10)
 
-download()
-validNums = getNums()
-if len(validNums) != len(set(validNums)):
-  print "Some office indices repeat!"
-#print validNums
-#print len(validNums)
-print validNums[0]
-downloadWait(593)
-print getWait(593)
+
+if __name__ == "__main__":
+  download_offices()
+  valid_nums = parse_office_nums()
+  assert len(valid_nums) == len(set(valid_nums)), "Some office indices repeat!"
+  print valid_nums[0]
+  print get_wait_time(593)  # San Mateo
